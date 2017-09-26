6d26c7b1ab5900e983f2e40d6b9932f6279ed894
Emmanuel Mar <emar772@gmail.com>
2017-09-26 08:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,3 @@ function whisper(string) {
 function logShout(string) {
   console.log("HELLO")
 }
-
-function logWhisper(string) {
-  console.log("hello")
-}
-
-function sayHiToGrandma(string) {
-  return string.toLowerCase("I can't hear you")
-  return string.touppercase("YES INDEED")
-  return string.toMixedCase("I love you, too")
-}
